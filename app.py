0e4d0d94a6f816ec6fd2aa9ee7c2375df7efa2b6
Randy Parra <randyparra@hotmail.com>
2025-04-03 21:47:27 -0400

    H-1 y H-2 listos

--- app.py
+++ app.py
@@ -6,7 +6,17 @@ app = Flask(__name__)
 CORS(app)
 
 
+@app.route("/users", methods=["GET"])
+def users():
+    return jsonify({
+        "payload": "success"
+    })
 
+@app.route("/user", methods=["DELETE"])
+def user():
+    return jsonify({
+        "payload": "success"
+    })
 
 
 if __name__ == "__main__":
